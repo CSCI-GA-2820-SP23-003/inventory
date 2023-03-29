0157e721ed64d12d41ef80ea84b15c74856572d1
shradhaiyer <shradha.thyagarajan@gmail.com>
2023-03-29 16:53:30 -0400

    added test case for querying with bad condition

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -353,3 +353,12 @@ class TestInventoryServer(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(data["status"], "OK")
+
+    def test_query_item_by_condition_wrong_condition(self):
+        """Querying list all with wrong condition should return 400"""
+        test_condition = "OLD"
+        response = self.client.get(
+            BASE_URL,
+            query_string=f"condition={quote_plus(test_condition)}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
