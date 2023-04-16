0f9df6dd6b2cce8eed2b4d9369510c2bd010311a
Rachit Jain <rachit.jain2706@gmail.com>
2023-04-15 23:15:37 -0400

    Update Inventory Item through UI

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -128,4 +128,51 @@ $(function () {
             flash_message("Server error!")
         });
     });
+
+    // ****************************************
+    // Update an Inventory Item
+    // ****************************************
+
+    $("#update-btn").click(function () {
+
+        let inventory_id = $("#inventory_id").val();
+        let name = $("#inventory_name").val();
+        let condition = $("#inventory_condition").val();
+        let quantity = $("#inventory_quantity").val();
+        let restock_level = $("#inventory_restock_level").val();
+
+        // Convert input type
+        if (!isNaN(quantity)) {
+            quantity = parseInt(quantity);
+        }
+        if (!isNaN(restock_level)) {
+            restock_level = parseInt(restock_level);
+        }
+
+        let data = {
+            "name": name,
+            "condition": condition,
+            "quantity": quantity,
+            "restock_level": restock_level
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "PUT",
+            url: `/inventory/${inventory_id}`,
+            contentType: "application/json",
+            data: JSON.stringify(data)
+        })
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
 })
